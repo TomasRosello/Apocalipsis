f399936617cb80e0126c5a4485882dbdea533d1e
mangelons <44055843+mangelons@users.noreply.github.com>
2018-10-25 12:53:35 +0200

    Holis

--- src/apocalipsis/Apocalipsis.java
+++ src/apocalipsis/Apocalipsis.java
@@ -15,7 +15,7 @@ public class Apocalipsis {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Holis
+        // Holis de fresis
     }
     
 }
